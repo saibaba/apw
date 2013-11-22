d865856af32a81875bfb46c7d788d265381f6cf8
Saibaba Telukunta <saibaba.telukunta@gmail.com>
2013-11-22 13:32:18 -0600

    cleaned up charting

--- lib/jqplotcharts_helper.rb
+++ lib/jqplotcharts_helper.rb
@@ -8,10 +8,11 @@ def data_to_string(data)
     return data.to_s()
 end
 
-def render_jqplot_scatter(data, name, label, width, height)
+def render_jqplot_scatter_x(data, name, label, width, height)
 
     btn_id = "btn_" + name
     div_id = "div_" + name
+
 
     raw_concat('<div id="' + div_id + '" style="height: ' + height.to_s + 'px;width: ' + width.to_s + 'px; "></div>')
     raw_concat('<button id="'+btn_id+'"style=";">Save ' + label + '</button><br/>')
@@ -21,12 +22,13 @@ def render_jqplot_scatter(data, name, label, width, height)
 
     raw_concat('var line = ' + data_to_string(data) + ';')
     raw_concat('var plot = $.jqplot("' + div_id + '", [line], {')
-    raw_concat('          title:"' + label + '",')
+    aaw_concat('          title:"' + label + '",')
     raw_concat('          series:[ { showLine:false, }],')
-    raw_concat('          seriesDefaults:{ renderer: $.jqplot.BubbleRenderer, rendererOptions: { bubbleGradient: true}, pointLabels: {show: true} },')
+    raw_concat('          seriesDefaults:{ renderer: $.jqplot.BubbleRenderer, rendererOptions: { autoscaleBubbles: false, bubbleGradient: true, bubbleAlpha: 0.5}, pointLabels: {show: true} },')
     raw_concat('          axes: {')
     raw_concat('              xaxis: {')
     raw_concat('                  label: "Risk Score",')
+    raw_concat('                  tickInterval: 0.02,')
     raw_concat('                  labelRenderer: $.jqplot.CanvasAxisLabelRenderer')
     raw_concat('              },')
     raw_concat('              yaxis: {')
@@ -43,7 +45,7 @@ def render_jqplot_scatter(data, name, label, width, height)
 
 end
 
-def render_jqplot_scatter_x(data, name, label, width, height)
+def render_jqplot_scatter(data, name, label, xlabel, ylabel, width, height)
 
     btn_id = "btn_" + name
     div_id = "div_" + name
@@ -56,12 +58,22 @@ def render_jqplot_scatter_x(data, name, label, width, height)
 
     raw_concat('var line = ' + data_to_string(data) + ';')
     raw_concat('var plot = $.jqplot("' + div_id + '", [line], {')
-    raw_concat('        title:"' + label + '",  series:[')
-    raw_concat('            { showLine:false,')
-    raw_concat('              markerOptions: { size: 7, style:"x" }')
-    raw_concat('            }')
-    raw_concat('        ]});')
+    raw_concat('        title:"' + label + '",' )
+    raw_concat('        seriesDefaults:{ rendererOptions: { }, pointLabels: { show: true } },')
+    raw_concat('        cursor:{ show: true , zoom: true },')
+    raw_concat('        series:[ { showLine:false, markerOptions: { size: 7, style:"x" } } ],')
+    raw_concat('        axes: { ')
+    raw_concat('              xaxis: {')
+    raw_concat('                  label: "' + xlabel + '",')
+    raw_concat('                  labelRenderer: $.jqplot.CanvasAxisLabelRenderer')
+    raw_concat('              },')
+    raw_concat('              yaxis: {')
+    raw_concat('                  label: "' + ylabel + '",')
+    raw_concat('                  labelRenderer: $.jqplot.CanvasAxisLabelRenderer')
+    raw_concat('              }')
+    raw_concat('        } ')
     raw_concat('    });')
+    raw_concat('});')
     raw_concat('$("#' + btn_id + '").click(function () {')
     raw_concat('           $("#' + div_id + '").jqplotSaveImage()')
     raw_concat('    });')
